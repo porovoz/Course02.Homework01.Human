64822bbd71e8d2acca1172b29f04e5a6f86841b5
porovoz <falkon1@list.ru>
2022-12-14 13:39:25 +0300

    Course02.Homework01.Human is done. Please check!

--- Human.java
+++ Human.java
@@ -1,42 +1,32 @@
 public class Human {
-    String name;
-    String town;
-    int yearOfBirth;
+    public String name;
+    public String town;
+    public int yearOfBirth;
 
-    String jobTitle;
+    public String jobTitle;
 
     public Human(String name, String town, int yearOfBirth, String jobTitle) {
         if (name == null || name.isEmpty()) {
-            this.name = "'The information is missing.'";
-        } else {
+            name = "'The information is missing.'";
+        }
             this.name = name;
-        }
         if (town == null || town.isEmpty()) {
-            this.town = "'The information is missing.'";
-        } else {
+            town = "'The information is missing.'";
+        }
             this.town = town;
+        if (yearOfBirth < 0) {
+            yearOfBirth = 0;
         }
-        this.yearOfBirth = Math.max(yearOfBirth, 0);
+        this.yearOfBirth = yearOfBirth;
         if (jobTitle == null || jobTitle.isEmpty()) {
-            this.jobTitle = "'The information is missing.'";
-        } else {
-            this.jobTitle = jobTitle;
+            jobTitle = "'The information is missing.'";
         }
+            this.jobTitle = jobTitle;
     }
-
-//    void checkForNullInformation() {
-//        if (name == null || town == null || jobTitle == null) {
-//            System.out.println("The information is missing.");
-//        } else {
-//            System.out.println("Hello! My name is " + name + ". " + "I'm from " + town + ". " + "I was born in " + yearOfBirth + ". " +
-//                    "I work for a position: " + jobTitle + ". " + "Nice to meet you!");
-//        }
-//    }
 
     @Override
     public String toString() {
         return "Hello! My name is " + name + ". " + "I'm from " + town + ". " + "I was born in " + yearOfBirth + ". " +
                 "I work for a position: " + jobTitle + ". " + "Nice to meet you!";
     }
-
 }
