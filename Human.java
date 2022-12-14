cb84250405216ea0f0554ad1532c7de48e0eedc6
porovoz <falkon1@list.ru>
2022-12-14 13:03:49 +0300

    Course02.Homework01.Human is done. Please check!

--- Human.java
+++ Human.java
@@ -1,2 +1,42 @@
 public class Human {
+    String name;
+    String town;
+    int yearOfBirth;
+
+    String jobTitle;
+
+    public Human(String name, String town, int yearOfBirth, String jobTitle) {
+        if (name == null || name.isEmpty()) {
+            this.name = "'The information is missing.'";
+        } else {
+            this.name = name;
+        }
+        if (town == null || town.isEmpty()) {
+            this.town = "'The information is missing.'";
+        } else {
+            this.town = town;
+        }
+        this.yearOfBirth = Math.max(yearOfBirth, 0);
+        if (jobTitle == null || jobTitle.isEmpty()) {
+            this.jobTitle = "'The information is missing.'";
+        } else {
+            this.jobTitle = jobTitle;
+        }
+    }
+
+//    void checkForNullInformation() {
+//        if (name == null || town == null || jobTitle == null) {
+//            System.out.println("The information is missing.");
+//        } else {
+//            System.out.println("Hello! My name is " + name + ". " + "I'm from " + town + ". " + "I was born in " + yearOfBirth + ". " +
+//                    "I work for a position: " + jobTitle + ". " + "Nice to meet you!");
+//        }
+//    }
+
+    @Override
+    public String toString() {
+        return "Hello! My name is " + name + ". " + "I'm from " + town + ". " + "I was born in " + yearOfBirth + ". " +
+                "I work for a position: " + jobTitle + ". " + "Nice to meet you!";
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+
+        Human maksim = new Human("Maksim", "Minsk", 1988, "brand-manager");
+        Human anya = new Human("Anya", "Moscow", 1993, "methodist of educational programs");
+        Human katya = new Human("Katya", "Kaliningrad", 1992, "product-manager");
+        Human artem = new Human("Atrem", "Moscow", 1995, "director of business development");
+
+        System.out.println(maksim);
+        System.out.println(anya);
+        System.out.println(katya);
+        System.out.println(artem);
+    }
 }
